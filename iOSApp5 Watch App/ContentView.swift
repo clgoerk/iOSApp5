70d90c18b17b7837f741c31a808bfd874f69299d
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-03-19 11:19:04 -0400

    Initial Commit

---
+++ iOSApp5 Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iOSApp5 Watch App
+//
+//  Created by Chris Goerk on 2025-03-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iOSApp5 Watch App/iOSApp5App.swift
@@ -0,0 +1,17 @@
+//
+//  iOSApp5App.swift
+//  iOSApp5 Watch App
+//
+//  Created by Chris Goerk on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct iOSApp5_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
