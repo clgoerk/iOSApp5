7462fb8c166dfc4b7c308b2e153c372b6e113211
clgoerk <Christopherlee.Goerk@triosstudent.com>
2025-03-20 08:25:03 -0400

    iOSApp5 timer and stopwatch on Apple Watch

---
+++ iOSApp5 Watch App/StopwatchView.swift
@@ -0,0 +1,18 @@
+//
+//  StopwatchView.swift
+//  iOSApp5 Watch App
+//
+//  Created by Chris Goerk on 2025-03-19.
+//
+
+import SwiftUI
+
+struct StopwatchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StopwatchView()
+}
---
+++ iOSApp5 Watch App/TimerView.swift
@@ -0,0 +1,18 @@
+//
+//  TimerView.swift
+//  iOSApp5 Watch App
+//
+//  Created by Chris Goerk on 2025-03-19.
+//
+
+import SwiftUI
+
+struct TimerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TimerView()
+}
